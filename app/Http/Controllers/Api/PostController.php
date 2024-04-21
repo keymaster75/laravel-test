1ab501ee99d39934189c2ea9f597c2fb0ce899a9
Aleksandar Aleksic <42803237+keymaster75@users.noreply.github.com>
2024-04-21 20:01:51 +0200

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'max:20'],
+            'content' => ['required', 'string', 'max:100'],
+        ]);
+        $post = $request->user()->posts()->create($validated);
+        return response()->json([
+            'post' => $post
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/user/{id}/{locale?}', [PostController::class, 'user'])->name('posts.user');
+Route::get('toggleFollow/{user}', [PostController::class, 'toggleFollow'])->middleware(['auth', 'verified'])->name('toggleFollow');
+Route::resource('posts', PostController::class)
+    ->only('create', 'store', 'edit', 'update', 'destroy')
+    ->middleware(['auth', 'verified']);
+Route::resource('posts', PostController::class)
+    ->only('show');
+// Route::get('/posts/{id}/{locale?}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile', [ProfileController::class, 'image'])->name('profile.image');
+});
+
+require __DIR__.'/auth.php';
